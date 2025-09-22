7e2277a77a50302326964d81eb7a99367cb590bc
Mustafa Albayrak <mustafa.albayrak@merzigo.local>
2025-09-22 16:20:41 +0300

    second commit

--- merzigo.bms.tennis/merzigo.bms.tennis/Controllers/OddSuggestionController.cs
+++ merzigo.bms.tennis/merzigo.bms.tennis/Controllers/OddSuggestionController.cs
@@ -22,6 +22,23 @@ namespace merzigo.bms.tennis.Controllers
 
             var h2hResult = await _apiService.GetH2H(player1Id, player2Id);
 
+            var standingsATP = await _apiService.GetStandings("ATP");
+            var standingsWTA = await _apiService.GetStandings("WTA");
+            var player1ATP = standingsATP.FirstOrDefault(p => p.playerKey == player1Id);
+            var player2ATP = standingsATP.FirstOrDefault(p => p.playerKey == player2Id);
+            var player1WTA = standingsWTA.FirstOrDefault(p => p.playerKey == player1Id);
+            var player2WTA = standingsWTA.FirstOrDefault(p => p.playerKey == player2Id);
+
+            // Null olmayanı seçiyoruz
+            var finalPlayer1 = player1ATP ?? player1WTA;
+            var finalPlayer2 = player2ATP ?? player2WTA;
+
+            var players1 = await _apiService.GetPlayers(player1Id);
+            var players2 = await _apiService.GetPlayers(player2Id);
+
+            var odds = await _apiService.GetOdds(matchId);
+            var liveOdds = await _apiService.GetLiveOdds(matchKey: matchId);
+
             var vm = new OddSuggestionViewModel
             {
                 FixturesPlayer1 = fixtures_for_player1 ?? new List<Fixtures>(),
@@ -30,7 +47,13 @@ namespace merzigo.bms.tennis.Controllers
                 Player1Name = player1,
                 Player2Name = player2,
                 Player1Id = player1Id,
-                Player2Id = player2Id
+                Player2Id = player2Id,
+                Player1Standing = finalPlayer1, // yeni ekleme
+                Player2Standing = finalPlayer2,  // yeni ekleme
+                Player1 = players1?.FirstOrDefault(),
+                Player2 = players2?.FirstOrDefault(),
+                Odds = odds,
+                LiveOdds = liveOdds
             };
 
             return View(vm);
--- merzigo.bms.tennis/merzigo.bms.tennis/Models/OddSuggestionViewModel.cs
+++ merzigo.bms.tennis/merzigo.bms.tennis/Models/OddSuggestionViewModel.cs
@@ -11,5 +11,14 @@ namespace merzigo.bms.tennis.Models
         public string? Player2Name { get; set; }
         public long Player1Id { get; set; }
         public long Player2Id { get; set; }
+
+        public Standings? Player1Standing { get; set; }
+        public Standings? Player2Standing { get; set; }
+
+        public Player? Player1 { get; set; }
+        public Player? Player2 { get; set; }
+
+        public merzigo.bms.tennis.Models.Odds.OddsResponse? Odds { get; set; }
+        public merzigo.bms.tennis.Models.Odds.LiveOddsResponse? LiveOdds { get; set; }
     }
 }
